6af422c872b3327c4159e65e8e17f9ccf52e775b
Yoshua1217 <joshuadkirsch@gmail.com>
2026-03-13 23:17:17 -0600

    Configure Next.js and GitHub Actions for Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+  // Trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, "");
+  
+  if (repo) {
+    assetPrefix = `/${repo}/`;
+    basePath = `/${repo}`;
+  }
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: basePath,
+  assetPrefix: assetPrefix,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
